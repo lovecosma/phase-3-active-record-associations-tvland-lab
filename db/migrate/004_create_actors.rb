3fcf7a797e00094fc444756b2dd4f06730e47d0e
love.cosma <williamsmatthew333@gmail.com>
2021-10-14 17:20:44 -0600

    Done.

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,5 +1,8 @@
 class CreateActors < ActiveRecord::Migration[6.1]
   def change
-    # Your code here
+     create_table :actors do |column|
+        column.string :first_name
+        column.string :last_name
+     end 
   end
 end
